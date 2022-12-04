2bb597a2e9a9565034d1994172997a42a725d75c
willelop <willelopz@gmail.com>
2022-12-04 18:52:43 +0100

    Added option to perform deep sleep.
    Added formatting improvements to main.c
    Deep sleep option needs a watchdog reboot

--- src/main.c
+++ src/main.c
@@ -25,12 +25,15 @@
 #include "http_client.h"
 #include "tiny-json.h"
 #include "lwip/apps/http_client.h"
+#include "pico/stdlib.h"
+#include "time.h"
 
 //File with custom config data
 #include "config.h"
 
+//#define POWERSAVE_SLEEP
 #define WAIT_WIFI_FAILURE_S 3
-#define SLEEP_TIME_S 120
+#define SLEEP_TIME_S 300
 
 int current_id = 0;
 char values [NUM_SENSORS][32];
@@ -43,9 +46,76 @@ void callback(const char * string);
 void callback_error(const char * error);
 void callback_closed(const char * error);
 
+#ifdef POWERSAVE_SLEEP
+#include <stdlib.h>
+#include "hardware/clocks.h"
+#include "hardware/structs/scb.h"
+#include "hardware/watchdog.h"
+#include "pico/sleep.h"
+#include "hardware/rtc.h"
+#include "hardware/rosc.h"
+static bool awake;
+void recover_from_sleep(uint scb_orig, uint clock0_orig, uint clock1_orig){
+
+    //Re-enable ring Oscillator control
+    rosc_write(&rosc_hw->ctrl, ROSC_CTRL_ENABLE_BITS);
+
+    //reset procs back to default
+    scb_hw->scr = scb_orig;
+    clocks_hw->sleep_en0 = clock0_orig;
+    clocks_hw->sleep_en1 = clock1_orig;
+
+    //reset clocks
+    clocks_init();
+    stdio_init_all();
+    irq_init_priorities();
+    return;
+}
+
+static void sleep_callback(void) {
+    printf("RTC woke us up\n");
+    uart_default_tx_wait_blocking();
+    awake = true;
+}
+
+static void rtc_sleep(void) {
+    datetime_t t = {
+            .year  = 2020,
+            .month = 06,
+            .day   = 05,
+            .dotw  = 5, // 0 is Sunday, so 5 is Friday
+            .hour  = 15,
+            .min   = 45,
+            .sec   = 00
+    };
+    datetime_t t_alarm = {
+            .year  = 2020,
+            .month = 06,
+            .day   = 05,
+            .dotw  = 5, // 0 is Sunday, so 5 is Friday
+            .hour  = 15,
+            .min   = 45,
+            .sec   = 30
+    };
+    // Start the RTC
+    rtc_init();
+    rtc_set_datetime(&t);
+    uart_default_tx_wait_blocking();
+    sleep_goto_sleep_until(&t_alarm, &sleep_callback);
+}
+#endif
+
 int main(void)
 
 {
+    stdio_init_all();
+    #ifdef POWERSAVE_SLEEP
+    if (watchdog_caused_reboot()) {
+        printf("Rebooted by Watchdog!\n");
+    } else {
+        printf("Clean boot\n");
+    }
+    #endif
     int reqnum = 0;
     uint16_t counter = 0;
     processing = false;
@@ -53,21 +123,21 @@ int main(void)
     char path[256];
     char counter_str[7];
     bool wifi_init = false;
-    stdio_init_all();
+  
     init_display();
     prepareBuffer();
     my_http_client_init(SERVER_IP, SERVER_PORT, &callback, &callback_error, &callback_closed);
 
-    while(1)
+    while (1)
     {
-        //Initialise Wifi if needed
-        if (wifi_init==false)
-        {                
+        // Initialise Wifi if needed
+        if (wifi_init == false)
+        {
             printf("Reinitialise Wifi");
             if (cyw43_arch_init_with_country(CYW43_COUNTRY_GERMANY))
             {
                 printf("WiFi init failed");
-                sleep_ms(WAIT_WIFI_FAILURE_S*1000);
+                sleep_ms(WAIT_WIFI_FAILURE_S * 1000);
                 continue;
             }
             cyw43_arch_enable_sta_mode();
@@ -75,50 +145,71 @@ int main(void)
             {
                 printf("failed to connect\n");
                 cyw43_arch_deinit();
-                sleep_ms(WAIT_WIFI_FAILURE_S*1000);
+                sleep_ms(WAIT_WIFI_FAILURE_S * 1000);
                 continue;
             }
             wifi_init = true;
         }
         connect();
-        for(int i =0; i < NUM_SENSORS; i++)
+        for (int i = 0; i < NUM_SENSORS; i++)
         {
             current_id = i;
             sprintf(&path[0], "/api/states/%s", requests[current_id]);
             processing = true;
-            request_website(path,token);
-            printf("Request Number %i\t",reqnum);
+            request_website(path, token);
+            printf("Request Number %i\t", reqnum);
             reqnum++;
-            while(processing)
+            while (processing)
             {
                 sleep_ms(100);
                 cyw43_arch_poll();
             }
         }
         close_connection();
-        //TODO Implement deactivation of the wifi during the sleep times
+        // TODO Implement deactivation of the wifi during the sleep times
         cyw43_arch_poll();
         puts("WiFi deinit");
         cyw43_arch_deinit();
         wifi_init = false;
 
         counter++;
-        sprintf(&counter_str[0],"%06d",counter);
-        //Now we paint the data that we downloaded
+        sprintf(&counter_str[0], "%06d", counter);
+        // Now we paint the data that we downloaded
         clear_buffer();
         add_counter(counter_str);
-        for(int i =0; i < NUM_SENSORS; i++)
+        for (int i = 0; i < NUM_SENSORS; i++)
         {
-            add_text(names[i],i, 1,false);
-            add_value(values[i],i, 0);
-            add_footer(update_times[i],i,1);
+            add_text(names[i], i, 1, false);
+            add_value(values[i], i, 0);
+            add_footer(update_times[i], i, 1);
         }
         add_grid();
         display_buffer();
+#ifdef POWERSAVE_SLEEP
+        uint scb_orig = scb_hw->scr;
+        uint clock0_orig = clocks_hw->sleep_en0;
+        uint clock1_orig = clocks_hw->sleep_en1;
+        printf("Switching to XOSC\n");
+        uart_default_tx_wait_blocking();
+        sleep_run_from_xosc();
+        printf("Switched to XOSC\n");
+        awake = false;
+        rtc_sleep();
+        // Make sure we don't wake
+        while (!awake)
+        {
+            printf("Should be sleeping\n");
+        }
 
-        //Sleeping time
+        recover_from_sleep(scb_orig, clock0_orig, clock1_orig);
+        puts("Enabling Watchdot to force reboot");
+        //Forcing reboot via watchdog
+        // watchdog_enable(100, 1);
+        // while(1);
+#else
         sleep_ms(SLEEP_TIME_S*1000);
-        puts("Waking up");
+        printf("Sleep finished\n");
+#endif
     }
 
     printf("End of program reached\n");
